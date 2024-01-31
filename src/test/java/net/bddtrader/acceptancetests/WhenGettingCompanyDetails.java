6068033116a911dc713445cff19929f8cbb63bd2
Olena Yurova <aly0naiur0va@gmail.com>
2024-01-31 23:42:50 +0100

    Using Path Parameters
    Updated whenGettingCompanyDetails with Path Parameters

--- src/test/java/net/bddtrader/acceptancetests/WhenGettingCompanyDetails.java
+++ src/test/java/net/bddtrader/acceptancetests/WhenGettingCompanyDetails.java
@@ -1,16 +1,39 @@
 package net.bddtrader.acceptancetests;
 
 import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
 
 public class WhenGettingCompanyDetails {
-    @Test
-    public void should_return_name_and_sector() {
-        RestAssured.get("http://localhost:9000/api/stock/aapl/company")
-                .then()
-                .body("companyName", equalTo("Apple, Inc."))
-                .body("sector", equalTo("Electronic Technology"));
+    @Before
+    public void base_URI() {
+        RestAssured.baseURI = "http://localhost:9000/api/";
     }
+//    @Test
+//    public void should_return_name_and_sector_in_production() {
+//    RestAssured.get("stock/aapl/company").then()
+//            .body("companyName", Matchers.equalTo("Apple, Inc."))
+//            .body("sector", Matchers.equalTo("Electronic Technology"));
+//    }
+    @Test
+    public void should_return_name_and_sector_locally() {
+        RestAssured.given()
+                .pathParam("symbol", "aapl")
+                .when()
+                .get("stock/{symbol}/company").then()
+                .body("companyName", Matchers.equalTo("Apple, Inc."))
+                .body("sector", Matchers.equalTo("Electronic Technology"));
+    }
+
+//    @Test
+//    public void should_return_name_and_sector() {
+//        RestAssured.get("http://localhost:9000/api/stock/aapl/company")
+//                .then()
+//                .body("companyName", equalTo("Apple, Inc."))
+//                .body("sector", equalTo("Electronic Technology"));
+//    }
 }
