b025df428eb0899e4e2c31085bfe26ce00db7c97
Olena Yurova <aly0naiur0va@gmail.com>
2024-02-02 18:31:35 +0100

    Sending data with POST queries exercises

--- src/test/java/net/bddtrader/unittests/clients/WhenCreatingANewClient.java
+++ src/test/java/net/bddtrader/unittests/clients/WhenCreatingANewClient.java
@@ -7,6 +7,9 @@ import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.equalTo;
@@ -19,13 +22,8 @@ public class WhenCreatingANewClient {
     @Test
     public void each_new_client_should_get_a_unique_id () {
         Client aNewClient = Client.withFirstName("Olena").andLastName("Yurova").andEmail("testemail@g.com");
-        String newClient = "{\n" +
-                "  \"email\": \"testemail@g.com\",\n" +
-                "  \"firstName\": \"Olena\",\n" +
-                "  \"lastName\": \"Yurova\"\n" +
-                "}";
         RestAssured.given().contentType(ContentType.JSON)
-                .body(newClient)
+                .body(aNewClient)
                 .when()
                 .post("/client")
                 .then().statusCode(200)
@@ -34,5 +32,46 @@ public class WhenCreatingANewClient {
                 .and().body("firstName", equalTo("Olena"))
                 .and().body("lastName", equalTo("Yurova"));
         }
+        /*
+         Exercise 1
+            Write a test to create a new client with the following details using the Client class:
+            First name: Jim
+            Last name: Halpert
+            Email: jim@halpert.com
+     */
+    @Test
+    public void create_a_new_client_with_specified_data_using_Client() {
+        Client theNewClient = Client.withFirstName("Jim").andLastName("Halpert").andEmail("jim@halpert.com");
+        given().contentType(ContentType.JSON).body(theNewClient)
+                .when().post("/client")
+                .then().statusCode(200)
+                .and().body("firstName", equalTo("Jim"))
+                .and().body("lastName", equalTo("Halpert"))
+                .and().body("email", equalTo("jim@halpert.com"));
+
+    }
+    /*
+        Exercise 2
+        Write another test to create a new client using a map with the following details:
+        First name: Kevin
+        Last name: Malone
+        Email: kevin@malone.com
+     */
+    @Test
+    public void create_a_new_client_with_specified_data_using_Map() {
+        Map<String, Object> clientData = new HashMap<>();
+        clientData.put("firstName", "Kevin");
+        clientData.put("lastName", "Malone");
+        clientData.put("email", "kevin@malone.com");
+
+        given().contentType(ContentType.JSON).body(clientData)
+                .when().post("/client")
+                .then().statusCode(200)
+                .and().body("firstName", equalTo("Kevin"))
+                .and().body("lastName", equalTo("Malone"))
+                .and().body("email", equalTo("kevin@malone.com"));
+    }
+
+
     }
 
